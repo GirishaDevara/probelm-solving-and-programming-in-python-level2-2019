039691f24fe500fc47a380597b0b2353f5db893e
girisha <girisha.devara@gmail.com>
2019-06-21 20:12:52 +0530

    upto date June 21 2019

---
+++ Packages/validators.py
@@ -0,0 +1,15 @@
+import re
+
+def phoneNumberValidater(number):
+    pattern = "^[6-9][0-9]{9}$|^[0][6-9][0-9]{9}$|^[+][9][1][6-9][0-9]{9}$"
+    if re.match(pattern,str(number)):
+        return True
+    else:
+        return False
+    return
+
+def emailValidater(email_id):
+    pattern = "^[0-9a-z][0-9a-z_.]{4,13}[0-9a-z][@][0-9a-z]{3,18}[.][a-z]{2,4}$"
+    if re.match(pattern,email_id):
+        return True
+    return False 
